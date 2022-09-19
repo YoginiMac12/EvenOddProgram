ad8d9ca203ad6e858254d072f5808979cb034c07
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-19 23:11:06 +0530

    Add files via upload

---
+++ EvenOddProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddProgram
+{
+     internal class Program
+    {
+       // static void Main(string[] args)
+       // {
+       //  int n;
+       // Console.WriteLine("Enter a Number");
+       //  n = Convert.ToInt32(Console.ReadLine());    
+
+       // if (n%2 == 0)
+       //  {
+       //    Console.WriteLine("Number is Even");
+       // }
+       // else
+           // {
+            //    Console.WriteLine("Number is Odd");
+            //}
+              //  Console.ReadLine();
+       // }
+    }
+}
